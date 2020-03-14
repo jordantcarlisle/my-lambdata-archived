f825e17bea9b7f263b0150ac0028d00903a6c6f7
jordantcarlisle <jordantcarlisle@gmail.com>
2020-03-14 12:31:38 -0500

    setup requirements and added second script

---
+++ dspt4-unit3-package/my_mod.py
@@ -0,0 +1,2 @@
+
+# dspt4-unit3-package/my_mod.py
--- dspt4-unit3-package/my_script.py
+++ dspt4-unit3-package/my_script.py
@@ -1,3 +1,9 @@
-# my_script.py
+
+# dspt4-unit3-package/my_script.py
+
+import pandas
 
 print("Happy Tuesday Night!")
+
+df = pandas.DataFrame({'x':[1,2,3], 'y':[4,5,6]})
+print(df.head())
