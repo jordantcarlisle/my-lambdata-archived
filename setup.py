8651d66e23944e81b6be5b2832f7953236d8fd66
jordantcarlisle <jordantcarlisle@gmail.com>
2020-03-16 21:36:15 -0500

    setup and package deployment files

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py file
+from setuptools import find_packages, setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name="dspt4_unit3_package",
+    version="1.0",
+    author="Jordan Carlisle",
+    author_email="jordantcarlisle@gmail.com",
+    description="My first python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/jordantcarlisle/my-lambdata",
+    #keywords="",
+    packages=find_packages() # ["dspt4_unit3_package"]
+)
