452469d3b82c405d7eece39cc70bdcfc75171401
jordantcarlisle <jordantcarlisle@gmail.com>
2020-03-18 22:43:26 -0500

    object oriented approach

--- dspt4_unit3_package/teams.py
+++ dspt4_unit3_package/teams.py
@@ -5,37 +5,41 @@ class Team():
         self.name = name
         self.city = city
 
-def advertise(my_team):
-    print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
+    def advertise(self):
+        print(f"HEY COME TO {self.city.upper()} TO SEE OUR GAMES!!!")
 
-def full_name(my_team):
-    return f"{my_team['city']} {my_team['name']}"
+    @property
+    def full_name(self):
+        return f"{self.city} {self.name}"
 
-# if __name__ == "__main__":
-#     teams = [
-#         {"city": "New York", "name": "Yankees"},
-#         {"city": "New York", "name": "Mets"},
-#         {"city": "Boston", "name": "Red Sox"},
-#         {"city": "New Haven", "name": "Ravens"},
-#         {"city": "Washington", "name": "Nationals"}
-#     ]
-#
-#     for team in teams:
-#         print("-------")
-#         print(full_name(team))
-#         advertise(team)
+if __name__ == "__main__":
+    teams = [
+        {"city": "New York", "name": "Yankees"},
+        {"city": "New York", "name": "Mets"},
+        {"city": "Boston", "name": "Red Sox"},
+        {"city": "New Haven", "name": "Ravens"},
+        {"city": "Washington", "name": "Nationals"}
+    ]
+
+    for team_attributes in teams:
+        team = Team(name=team_attributes['name'], city=team_attributes['city'])
+        print("-------")
+        # print(full_name(team))
+        # advertise(team)
+        print(team.full_name)
+        team.advertise()
 
 # df1 = DataFrame({'x': [1,2,3], 'y': [4,5,6]})
 # df2 = DataFrame({'x': [7,7,7], 'y': [4,4,4]})
 
-team = Team(city="Washington", name="Wizards") # initialize / create instance
-print(team)
-print(type(team))
-print(team.name) # invoking attributes
-print(team.city) # invoking attributes
-
-team2 = Team(city="New York", name="Giants") # initialize / create instance
-print(team2)
-print(type(team2))
-print(team2.name) # invoking attributes
-print(team2.city) # invoking attributes
+# team = Team(city="Washington", name="Wizards") # initialize / create instance
+# print(team)
+# print(type(team))
+# print(team.name) # invoking attributes
+# print(team.city) # invoking attributes
+#
+# team2 = Team(city="New York", name="Giants") # initialize / create instance
+# print(team2)
+# print(type(team2))
+# print(team2.name) # invoking attributes
+# print(team2.city) # invoking attributes
