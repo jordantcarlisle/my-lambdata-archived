d19b39df789202bb7fdd72e22872325d22d648db
jordantcarlisle <jordantcarlisle@gmail.com>
2020-03-21 17:42:14 -0500

    created class with OOP approach w/ inheritance

--- dspt4_unit3_package/assignment.py
+++ dspt4_unit3_package/assignment.py
@@ -1,23 +1,54 @@
-# my_lambdata/assignment.py (functional approach)
+# my_lambdata/assignment.py (OOP approach w/ inheritance)
 
 import pandas
 
-# State abbreviations
+class CustomFrame(pandas.DataFrame):
 
+    """
 
-# State abbreviation -> Full Name and visa versa. FL -> Florida, etc..
-# (Handle Washington DC and territories like Puerto Rico etc..)
-# take pandas dataframe that has state abbreviations
-# ... and write a function to add corresponding state names
+    Param: my_df (pandas.DataFrame) containing a column called "abbrev"
 
-def add_state_names():
-  pass
+    """
+
+    def add_state_names(self):
+
+        """
+
+        Adds corresponding state names to a dataframe.
+
+        """
+
+        # type(new_df["abbrev"]) #> <class 'pandas.core.series.Series'>
+        # see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+
+        names_map = {
+            "CA": "Cali",
+            "CT": "Conn",
+            "CO": "Colorado"
+            # todo: more abbrevs!
+        }
+        self["name"] = self["abbrev"].map(names_map)
 
 
 if __name__ == "__main__":
+    print("--------------")
 
-    df = pandas.DataFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC"]})
-    print(df.head())
+    #df1 = pandas.DataFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC"]})
+    #print(df1.head())
+    #new_df = add_state_names(df1)
+    #print(new_df.head())
+    #processor = DataProcessor(df1)
+    #print(processor.df.head())
+    #processor.add_state_names()
+    #print(processor.df.head())
 
-    df2 = pandas.DataFrame({"abbrev": ["OH", "MI", "CO", "TX", "DC"]})
-    print(df2.head())
+    custom_df = CustomFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC"]})
+    print(custom_df.head())
+    custom_df.add_state_names()
+    print(custom_df.head())
+
+    #print("--------------")
+    #df2 = pandas.DataFrame({"abbrev": ["OH", "MI", "CO", "TX", "PA"]})
+    #print(df2.head())
+    #new_df2 = add_state_names(df2)
+    #print(new_df2.head())
